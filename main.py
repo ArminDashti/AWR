cab67e9e5bfb6060702d4128e7cbf32bcd12c563
Armin <arminonline71@gmail.com>
2024-05-15 09:40:06 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 20:55:28 2024
+
+@author: Armin
+"""
+
+class AWR:
+    def __init__(self):
+        pass
+    
+    
+    def build_nets(self):
+        pass
+    
+    
+    def update_normalizers(self):
+        pass
+    
+    
+    def calculate_return(self):
+        pass
+    
+    def calculate_advantage(self):
+        pass
+    
+    def calculate_advantage_weight(self):
+        pass
+    
+    def sample_action(self):
+        pass
+    
+    
+    def train(self):
+        time_step = 0
+        while (time_step < max_iter):
+            pass
+        
+        
+    
